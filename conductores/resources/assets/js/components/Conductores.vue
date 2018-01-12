149013411519824d828fbea8a57c8bc6cee9ad9a
vicmans <vsanchezl94@gmail.com>
2018-01-12 00:13:56 -0400

    Nuevo componente Conductores

---
+++ conductores/resources/assets/js/components/Conductores.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="row">
+
+            <div class="alert alert-info" v-if="conductores.lenght<=0">
+                NO hay resultados
+            </div>
+
+            
+                  <div v-for="conduct in conductores" class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail conduct">
+
+                  <!-- faltan las fotos -->
+
+                      <div class="caption">
+                        <h3>{{ conduct.name }} {{ conduct.last_name }}</h3>
+                        <p> {{ conduct.short }} </p>
+
+                        <p> <a :href="'conducts/'+conduct.id" class="btn btn-primary" role="button">VER</a> <a href="#" class="btn btn-default" role="button">CONTACTAR</a> </p>
+                      </div>
+
+                    </div>
+
+                  </div>
+
+            </div>
+        
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        mounted() {
+            axios.get('/conductores')
+            .then(res => {
+                this.conductores = res.data;
+                console.log(res.data)
+            });
+            console.log('Component mounted.')
+        },
+        data(){
+            return {
+                conductores: []
+            }
+        }
+    }
+</script>
